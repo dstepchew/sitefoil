a43ba5148c330b41dcb685b7693a17cb20d6016d
Dan Stepchew <dstepchew@gmail.com>
2014-07-12 15:05:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sitefoil::Application.config.secret_key_base = '4b4d5f4cd19dd0c3b640881cf2842106a27baaad3f7691e4a05d618799d61df1f4142160c32110f1a04704fa1c1ebdc9ebb297e66199ca36ce2f4ee45328586e'
