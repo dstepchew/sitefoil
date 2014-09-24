8bba311ae8ef24cde19f1c987fa427dbd594583c
Nazar Kuliyev <nazar.kuliev@gmail.com>
2014-09-24 15:20:35 +0600

    fix for production

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,7 @@
 #
 
 class User < ActiveRecord::Base
+	attr_protected []
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
