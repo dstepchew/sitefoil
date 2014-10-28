fc9a0cfcf26ffd573a086611f80d00ed9647352d
Nazar Kuliyev <nazar.kuliev@gmail.com>
2014-10-29 04:03:45 +0600

    -

---
+++ app/controllers/test_site_controller.rb
@@ -0,0 +1,2 @@
+class TestSiteController < ApplicationController
+end
---
+++ test/controllers/test_site_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TestSiteControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
