596b999d69643ac10abc26f30de1745cc9535ff0
Nazar Kuliyev <nazar.kuliev@gmail.com>
2014-11-05 16:31:10 +0600

    -

---
+++ app/controllers/test_site_controller.rb
@@ -0,0 +1,5 @@
+class TestSiteController < ApplicationController
+  def index
+  	render layout: false
+  end
+end
---
+++ test/helpers/test_site_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class TestSiteHelperTest < ActionView::TestCase
+end
